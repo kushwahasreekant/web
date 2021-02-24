6d9645c87fea9cf539a82fbee78201c224bda99c
shreekant <kushwahasree@gmail.com>
2021-02-24 09:31:01 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("hello app");
+var x =80;
+console.log(x);
+console.log("line1");
+//setTime function(){
+//console.log("line2")},100)
+//console.log("line3");
+//console.log("line4");
+
+//const value1 =parseInt(prompt("enter the value"));
+//const value2 =parseInt(prompt("enter the value2"));
+//alert(value1+value2);
+
+function showAdd(){
+    const value1 =parseInt(prompt("enter the value"));
+const value2 =parseInt(prompt("enter the value2"));
+alert(value1+value2);
+
+}
